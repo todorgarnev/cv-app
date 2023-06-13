2de5e8e23013b985958be0075b002d7ed9d3d27b
Todor Garnev <tgarnev@abv.bg>
2023-06-13 16:31:49 +0300

    Added slides component

---
+++ src/lib/deck/slides.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import Reveal from "reveal.js";
+	import Markdown from "reveal.js/plugin/markdown/markdown";
+	import Highlight from "reveal.js/plugin/highlight/highlight";
+
+	import "reveal.js/dist/reveal.css";
+	import "reveal.js/dist/theme/black.css";
+	import "reveal.js/plugin/highlight/monokai.css";
+
+	onMount(() => {
+		const deck = new Reveal({
+			autoAnimateEasing: "ease",
+			autoAnimateDuration: 1,
+			hash: true,
+			controls: true,
+			progress: true
+		});
+
+		deck.initialize({
+			plugins: [Markdown, Highlight]
+		})
+	})
+</script>
+
+<div class="reveal">
+	<div class="slides">
+		<section>Slide 1</section>
+		<section>Slide 2</section>
+	</div>
+</div>
